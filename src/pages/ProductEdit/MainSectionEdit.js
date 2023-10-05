f7261d4c1c7d71a8fac5cff16b15dc2d35cf9255
Harriet Oteng <harriet.oteng@gmail.com>
2023-10-05 14:21:47 +0000

    Add react-quill

--- src/pages/ProductEdit/MainSectionEdit.js
+++ src/pages/ProductEdit/MainSectionEdit.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useLocation } from 'react-router-dom';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+// import 'react-quill/dist/quill.core.css';
 import { PiLaptopLight, PiTrashLight, PiCheckBold } from 'react-icons/pi';
 import styles from '../../styles/ProdView.module.css';
 import MainUserInfo from '../../components/MainUserInfo';
@@ -11,18 +14,25 @@ export default function MainSectionEdit() {
   const location = useLocation();
   const data = location.state?.productInfo;
 
+  const [description, setDescription] = useState(data.desc);
+  const [title, setTitle] = useState(data.name);
+
   return (
     <section className={`${styles.productDetails} ${styles.sectionContainers}`}>
       <div className={styles.product}>
+        {/* <div className={styles.imgCont}> */}
         <img className={styles.productImage} src={data.image} alt="loftOS" />
-        <span>
+        <span className={styles.category}>
           <PiLaptopLight className={styles.softwareIcon} style={{ backgroundColor: `${appConfData?.mainColor}` }} />
           {data.type}
         </span>
         <PiTrashLight className={`${styles.trashIcon}`} />
-        <h3><input name="name" type="text" required defaultValue={data.name} className={styles.videoInput} /></h3>
+        {/* </div> */}
+        <h3><input name="name" type="text" required value={title} onChange={setTitle} className={styles.videoInput} /></h3>
         {/* value={data.name} */}
-        <p>{data.desc}</p>
+        <div>
+          <ReactQuill theme="snow" value={description} className={styles.richEdit} onChange={setDescription} />
+        </div>
         <div className={styles.editBtnCont}>
           <button type="button">Cancel</button>
           <button type="button" className={styles.saveBtn}>
