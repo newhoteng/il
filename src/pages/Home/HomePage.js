26c061ff03a83f70da7f28b7641ed8869d5f1802
Harriet Oteng <harriet.oteng@gmail.com>
2023-10-06 09:53:58 +0000

    Resize product image

--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -3,7 +3,7 @@ import React from 'react';
 function HomePage() {
   return (
     <main>
-      <h1 className="min-h-[calc(100lvh-55px)] text-2xl text-center text-[#aeadad] flex items-center justify-center px-1">The home page is under construction. Check out the product page and while there click on the &apos;Edit&apos; button</h1>
+      <h1 className="min-h-[calc(100lvh-55px)] text-2xl text-center text-[#aeadad] flex items-center justify-center px-4">The home page is under construction. Check out the product page and while there click on the &apos;Edit&apos; button</h1>
     </main>
   );
 }
